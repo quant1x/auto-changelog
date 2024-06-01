198ee629ad3739c8025e45067ddc84c382a65f85
wangfeng <wangfengxy@sina.cn>
2024-06-01 18:08:49 +0800

    屏蔽git push的操作

--- main.go
+++ main.go
@@ -225,8 +225,8 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%+v\n", obj)
-	err = r.Push(&git.PushOptions{})
-	fmt.Printf("%+v\n", err)
+	//err = r.Push(&git.PushOptions{})
+	//fmt.Printf("%+v\n", err)
 	h, err := r.Head()
 	if err != nil {
 		fmt.Printf("get HEAD error: %s", err)
@@ -237,5 +237,9 @@ func main() {
 	_, err = r.CreateTag(tag, h.Hash(), &git.CreateTagOptions{
 		Message: message,
 	})
-	fmt.Printf("%+v\n", err)
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	} else {
+		fmt.Println("OK.")
+	}
 }
