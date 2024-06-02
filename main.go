1dac9bcf4fb54d9daedd09473fbfb6bab5ba1a76
wangfeng <wangfengxy@sina.cn>
2024-06-02 15:31:35 +0800

    拆分模板常量

--- main.go
+++ main.go
@@ -17,25 +17,6 @@ const (
 	changeLogFilename     = "CHANGELOG.md"
 	commitUpdateChangeLog = "update changelog"
 	defaultFirstVersion   = "0.0.0"
-	templateChangeLog     = `# Changelog
-All notable changes to this project will be documented in this file.
-
-## [Unreleased]
-
-{{ range .Versions}}
-## [{{.Version}}] - {{.Date}}
-### Changed
-{{- range .Commits}}
-- {{.Message}}
-{{- end}}
-{{ end}}
-
-[Unreleased]: {{.RepositoryURL}}/compare/v{{.Latest}}...HEAD
-{{- range .Versions}}
-{{if ne .Version .Oldest}}[{{.Version}}]: {{.RepositoryURL}}/compare/v{{.Previous}}...v{{.Version}}{{- end}}
-{{- end}}
-[{{.Oldest}}]: {{.RepositoryURL}}/releases/tag/v{{.Oldest}}
-`
 )
 
 func main() {
---
+++ template.go
@@ -0,0 +1,23 @@
+package main
+
+const (
+	templateChangeLog = `# Changelog
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+
+{{ range .Versions}}
+## [{{.Version}}] - {{.Date}}
+### Changed
+{{- range .Commits}}
+- {{.Message}}
+{{- end}}
+{{ end}}
+
+[Unreleased]: {{$.RepositoryURL}}/compare/v{{.Latest}}...HEAD
+{{- range .Versions}}
+{{if ne .Version .Oldest}}[{{.Version}}]: {{$.RepositoryURL}}/compare/v{{.Previous}}...v{{.Version}}{{- end}}
+{{- end}}
+[{{.Oldest}}]: {{$.RepositoryURL}}/releases/tag/v{{.Oldest}}
+`
+)
