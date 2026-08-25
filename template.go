6abac8e8f5f5cffbdd2c66dc123bb7812c0653a9
wangfeng <wangfengxy@sina.cn>
2026-08-26 07:42:26 +0800

    修复 CHANGELOG 模板排版，标题与列表间补充空白行以符合 markdownlint 规范

--- template.go
+++ template.go
@@ -2,17 +2,18 @@ package main
 
 const (
 	templateChangeLog = `# Changelog
+
 All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 {{ range .Versions}}
 ## [{{.Version}}] - {{.Date}}
+
 ### Changed
-{{- range .Commits}}
+{{ range .Commits}}
 - {{.Message}}
 {{- end}}
 {{ end}}
-
 [Unreleased]: {{$.RepositoryURL}}/compare/v{{.Latest}}...HEAD
 {{- range .Versions}}
 {{if ne .Version .Oldest}}[{{.Version}}]: {{$.RepositoryURL}}/compare/v{{.Previous}}...v{{.Version}}{{- end}}
