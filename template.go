bda64c2d89fde050e5f04b46805b7f3a7e07751f
wangfeng <wangfengxy@sina.cn>
2026-08-26 08:00:09 +0800

    恢复旧模板

--- template.go
+++ template.go
@@ -2,16 +2,17 @@ package main
 
 const (
 	templateChangeLog = `# Changelog
-
 All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 {{ range .Versions}}
 ## [{{.Version}}] - {{.Date}}
-{{ range .Commits}}
+### Changed
+{{- range .Commits}}
 - {{.Message}}
 {{- end}}
 {{ end}}
+
 [Unreleased]: {{$.RepositoryURL}}/compare/v{{.Latest}}...HEAD
 {{- range .Versions}}
 {{if ne .Version .Oldest}}[{{.Version}}]: {{$.RepositoryURL}}/compare/v{{.Previous}}...v{{.Version}}{{- end}}
